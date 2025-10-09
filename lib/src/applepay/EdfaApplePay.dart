8550ba57a3551898cd2d85e8dbeac13c550ff062
aaxf1 <aaxf11@gmail.com>
2025-10-09 10:39:07 +0300

    Update EdfaApplePay.dart

--- lib/src/applepay/EdfaApplePay.dart
+++ lib/src/applepay/EdfaApplePay.dart
@@ -5,7 +5,7 @@ import 'package:edfapg_sdk/src/request/EdfaPgSaleOrder.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:edfapg_sdk/src/Helpers.dart';
 
-class EdfaApplePay{
+class EdfaApplePay {
   Function(Map response)? _onTransactionFailure;
   Function(Map response)? _onTransactionSuccess;
   Function(Map response)? _onAuthentication;
@@ -14,82 +14,85 @@ class EdfaApplePay{
 
   EdfaPgSaleOrder? _order;
   EdfaPgPayer? _payer;
-  String? _applePayMerchantID;
 
-  EdfaApplePay setOrder(EdfaPgSaleOrder order){
+  // تم حذف applePayMerchantID لأنها لم تعد مطلوبة أو مدعومة
+  // String? _applePayMerchantID;
+
+  EdfaApplePay setOrder(EdfaPgSaleOrder order) {
     _order = order;
     return this;
   }
 
-  EdfaApplePay setPayer(EdfaPgPayer payer){
+  EdfaApplePay setPayer(EdfaPgPayer payer) {
     _payer = payer;
     return this;
   }
 
-  EdfaApplePay setApplePayMerchantID(String applePayMerchantID){
+  // تم حذف setApplePayMerchantID بالكامل لأنه لم يعد يستخدم
+  /*
+  EdfaApplePay setApplePayMerchantID(String applePayMerchantID) {
     _applePayMerchantID = applePayMerchantID;
     return this;
   }
+  */
 
-  EdfaApplePay onTransactionFailure(Function(Map response) callback){
+  EdfaApplePay onTransactionFailure(Function(Map response) callback) {
     _onTransactionFailure = callback;
     return this;
   }
 
-  EdfaApplePay onTransactionSuccess(Function(Map response) callback){
+  EdfaApplePay onTransactionSuccess(Function(Map response) callback) {
     _onTransactionSuccess = callback;
     return this;
   }
 
-  EdfaApplePay onAuthentication(Function(Map response) callback){
+  EdfaApplePay onAuthentication(Function(Map response) callback) {
     _onAuthentication = callback;
     return this;
   }
 
-  EdfaApplePay onError(Function(Map response) callback){
+  EdfaApplePay onError(Function(Map response) callback) {
     _onError = callback;
     return this;
   }
 
-  EdfaApplePay onPresent(Function(BuildContext context) callback){
+  EdfaApplePay onPresent(Function(BuildContext context) callback) {
     _onPresent = callback;
     return this;
   }
 
-
-  initialize(BuildContext context){
-
+  initialize(BuildContext context) {
     EdfaPgSdk.instance.ADAPTER.APPLE_PAY.execute(
-        applePayMerchantId: _applePayMerchantID ?? "",
-        order: _order!,
-        payer: _payer!,
-        callback: ApplePayResponseCallback(
-            authentication: (Map response) {
-              Log(response.toString());
-              _onAuthentication!(response);
-            },
-            success: (Map response){
-              Log(response.toString());
-              _onTransactionSuccess!(response);
-            },
-            failure: (Map response){
-              Log(response.toString());
-              _onTransactionFailure!(response);
-            },
-            error: (Map error){
-              _onError!(error);
-            },
-        )
+      // تمت إزالة applePayMerchantId لأنها غير مطلوبة الآن
+      order: _order!,
+      payer: _payer!,
+      callback: ApplePayResponseCallback(
+        authentication: (Map response) {
+          Log(response.toString());
+          _onAuthentication?.call(response);
+        },
+        success: (Map response) {
+          Log(response.toString());
+          _onTransactionSuccess?.call(response);
+        },
+        failure: (Map response) {
+          Log(response.toString());
+          _onTransactionFailure?.call(response);
+        },
+        error: (Map error) {
+          _onError?.call(error);
+        },
+      ),
     );
 
     Future.delayed(const Duration(milliseconds: 200)).then((value) {
-      if(_onPresent != null) {
+      if (_onPresent != null) {
         _onPresent!(context);
       }
     });
   }
 
-  Widget widget(){
+  Widget widget() {
     return const SizedBox();
   }
 }
